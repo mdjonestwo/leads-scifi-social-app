2134045b61bec487d25b7c84d9dd6aa38cc68366
mdjonestwo <mdjones2@protonmail.com>
2021-10-09 23:36:10 -0400

    added next video functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,7 @@
-
-
-
     const quotesEl = document.querySelector('.quotes');
     const loaderEl = document.querySelector('.loader');
 
-    // get the CLIPS from API
+    //GET all the clips from API
     function getClips(){
         var settings = {
             url: "/api/clips",
@@ -32,6 +29,7 @@
 
     var i = -1;
 
+       
     //Get One Clip 
     function getOneClip(){
         var settings = {
@@ -45,30 +43,50 @@
             n++;
             return n;
         }
+
         i=increment(i);
-            
-        $("#next-post").append(
-            `<div class="post-square">
-            <div class="square">
-            </div>
-            <div class="title">
-                <h3>${response[i].Title}</h3>
-            </div>
-            <div class="user"><h4>${response[i].User}</h4></div>
-            <div class="caption"><p>${response[i].Caption}</p></div>
-            </div>`
-        );
+        console.log(response[i])
+        console.log(i)
+
+        if (response[i] === undefined){
+            $(window).unbind('scroll');
+            console.log("unbind")
+        }else{
+            $("#next-post").append(
+                `<div class="post-square" id=${i}>
+                <div class="square">
+                ${response[i].Video}
+                </div>
+                <div class="title">
+                    <h3>${response[i].Title}</h3>
+                </div>
+                <div class="user"><h4>${response[i].User}</h4></div>
+                <div class="caption"><p>${response[i].Caption}</p></div>
+                <div class="btn-row">
+                <a onclick="">Keep Watching</a>
+                <a id='next' class="next" href="#1" onclick="nextVideo()">Next Video</a>
+                </div>
+                </div>`
+            );
+        }
         })
     };
 
-    // Trigger Get One Cip On Scroll Event 
+    // Trigger Get One Cip after 3 seconds on Scroll Event 
         $(window).scroll(function() {
             if($(window).scrollTop() + $(window).height() > $(document).height() - 5) {
-                // $(window).unbind('scroll');
-                // alert("near bottom!");
-                getOneClip()
+                setTimeout(function(){getOneClip()},3000);
             }
          });
-        
+
+
+    //Play next video 
+        function nextVideo(){
+            var x = i++;
+            $('.next').attr("href", `#${x}`)
+        }   
+
+  
+
 
     
