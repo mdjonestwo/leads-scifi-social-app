ed53cc536d486e41efb78fb5dc3e0d1742dad969
mdjonestwo <mdjones2@protonmail.com>
2022-04-09 18:24:39 -0400

    updated video size

--- leads-front-end/src/App.js
+++ leads-front-end/src/App.js
@@ -5,7 +5,7 @@ import Video from './Video.js';
 import myVideo1 from "./pexels-tima-miroshnichenko-6498520.mp4"
 import myVideo2 from "./pexels-c-technical-6153725.mp4"
 import myVideo3 from "./pexels-mikhail-nilov-8239190.mp4"
-import videojs from 'video.js'
+
 
 function App() {
   // Axios({
@@ -57,7 +57,7 @@ function App() {
 
           />
             <Video
-            url= "https://v39.tiktokcdn-us.com/40ad6bc94b9448c3196705dcd493085f/6184978f/video/tos/useast2a/tos-useast2a-ve-0068c003/2c90b46c15f542918f68b689a1ef7f1d/?a=1233&amp;br=3626&amp;bt=1813&amp;cd=0%7C0%7C0&amp;ch=0&amp;cr=0&amp;cs=0&amp;dr=0&amp;ds=2&amp;er=&amp;ft=Sduyqeta4kag3&amp;l=2021110420312301011313512820AB6F34&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=ampsZTs6ZjpwODMzNzczM0ApN2ZmN2RkOztoNzo5OzxkOmdjLS1xcjRvZGtgLS1kMTZzc180Ly8wLzI2MWBhYDAzYV46Yw%3D%3D&amp;vl=&amp;vr="
+            url={myVideo3}
             channel= "innov8"
             description= "In a world where drones are only issued for goverment use to monitor civilians Khadijja builds one on her own... "
             movieTitle= "Similar"
--- leads-front-end/src/VideoSidebar.js
+++ leads-front-end/src/VideoSidebar.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 import "./VideoSidebar.css"
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-// import MessageIcon from '@mui/icons-material/Message';
-// import ShareIcon from '@mui/icons-material/Share';
+import MessageIcon from '@mui/icons-material/Message';
+import ShareIcon from '@mui/icons-material/Share';
 
 function VideoSidebar({ likes, shares, messages }) {
     const [liked, setLiked] = useState(false);
 
     return (
         <div className='videoSidebar'>
-            <div className="videoSidebar_button">
+         \<div className="videoSidebar_button">
                 {liked ? (
                     <FavoriteIcon fontSize='large' 
                     onClick={(e) => setLiked(false)}/>  
@@ -23,14 +23,14 @@ function VideoSidebar({ likes, shares, messages }) {
                
                 <p>{liked ? likes + 1 : likes}</p>
             </div>
-            {/* <div className="videoSidebar_button">
+         <div className="videoSidebar_button">
                 <MessageIcon fontSize="large" /> 
                 <p>{messages}</p>
             </div>
             <div className="videoSidebar_button">
                 <ShareIcon fontSize="large" /> 
                 <p>{shares}</p>
-            </div> */}
+            </div> 
         </div>
     )
 }
